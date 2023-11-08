5e099ceecf46c63bff1fb0cffe2d6d77c55c28a9
LIWenShuo <1064798423@qq.com>
2023-11-08 10:03:06 +0800

    init

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+import Link from 'next/link';
+export default function Page() {
+  return '登录页'
+}
